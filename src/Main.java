abd8c8cf232c5141f98f47a474419f9f37010b49
kalebanagnostou <anagnostou.kaleb@gmail.com>
2025-08-09 21:33:40 -0700

    GUI Draft

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.stage.StageStyle;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("View/Resources/style.css").toExternalForm());
+
+        primaryStage.setTitle("Data Damage");
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
