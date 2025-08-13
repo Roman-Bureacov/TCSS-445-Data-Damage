1fa3459ee2528e172942da04e2eb012a66ae8546
RomaBureacov <RomanBureacov@gmail.com>
2025-08-12 17:01:38 -0700

    documentation modification

--- src/model/script/TimeSheet.java
+++ src/model/script/TimeSheet.java
@@ -14,9 +14,9 @@ public class TimeSheet implements Iterable<Object[]> {
     private final List<Object[]> timesheet = new ArrayList<>();
 
     /**
-     * Writes an event into the time sheet from the playhead and
+     * Writes an event into the time sheet from the playhead and then
      * advances the playhead.
-     * @param timeDelta the time delta from the current event playhead
+     * @param timeDelta the time delta to advance the playhead in milliseconds
      * @param damage the damage caused during the event
      * @param tags the associated tags with the event
      */
