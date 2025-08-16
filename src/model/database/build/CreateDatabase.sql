81fcfe5273da3a14f17b318b8fdf45cd63f0ff8d
kalebanagnostou <anagnostou.kaleb@gmail.com>
2025-08-16 15:29:20 -0700

    CREATE DATABASE SQL

--- src/model/database/build/CreateDatabase.sql
+++ src/model/database/build/CreateDatabase.sql
@@ -1,19 +1,19 @@
 CREATE TABLE weapons (
-    weapon_id INT AUTO_INCREMENT PRIMARY KEY,
+    weapon_id INTEGER PRIMARY KEY,
     weapon_type TEXT NOT NULL,
     frame TEXT NOT NULL
 );
 
 CREATE TABLE weapon_stats (
-    weapon_id INT,
-    reserves INT DEFAULT 0 CHECK (reserves >= 0),
-    fire_rate INT DEFAULT 0 CHECK (fire_rate >= 0),
-    reload_speed INT DEFAULT 0 CHECK (reload_speed >= 0),
-    magazine INT DEFAULT 0 CHECK (magazine >= 0),
-    stow_speed INT DEFAULT 0,
-    ready_speed INT DEFAULT 0,
-    body_damage INT DEFAULT 0 CHECK (body_damage >= 0),
-    precision_damage INT DEFAULT 0 CHECK (precision_damage >= 0),
+    weapon_id INTEGER,
+    reserves INTEGER DEFAULT 0 CHECK (reserves >= 0),
+    fire_rate INTEGER DEFAULT 0 CHECK (fire_rate >= 0),
+    reload_speed INTEGER DEFAULT 0 CHECK (reload_speed >= 0),
+    magazine INTEGER DEFAULT 0 CHECK (magazine >= 0),
+    stow_speed INTEGER DEFAULT 0,
+    ready_speed INTEGER DEFAULT 0,
+    body_damage INTEGER DEFAULT 0 CHECK (body_damage >= 0),
+    precision_damage INTEGER DEFAULT 0 CHECK (precision_damage >= 0),
     FOREIGN KEY (weapon_id)
         REFERENCES weapons (weapon_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
@@ -21,7 +21,7 @@ CREATE TABLE weapon_stats (
 );
 
 CREATE TABLE slotability (
-    weapon_id INT,
+    weapon_id INTEGER,
     in_kinetic BOOLEAN DEFAULT FALSE,
     in_energy BOOLEAN DEFAULT FALSE,
     in_power BOOLEAN DEFAULT FALSE,
@@ -32,7 +32,7 @@ CREATE TABLE slotability (
 );
 
 CREATE TABLE weapon_info (
-    weapon_id INT,
+    weapon_id INTEGER,
     weapon_desc TEXT,
     image BLOB,
     FOREIGN KEY (weapon_id)
@@ -42,7 +42,7 @@ CREATE TABLE weapon_info (
 );
 
 CREATE TABLE modifiers (
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
 mod_name VARCHAR(64) NOT NULL,
 damage_mult DOUBLE DEFAULT 1,
 fire_rate_mult DOUBLE DEFAULT 1,
@@ -52,13 +52,13 @@ precision_damage_mult DOUBLE DEFAULT 1
 );
 
 CREATE TABLE sims (
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INTEGER AUTO_INCREMENT PRIMARY KEY,
 save_name VARCHAR(64) UNIQUE,
-kinetic INT,
-energy INT,
-power INT,
+kinetic INTEGER,
+energy INTEGER,
+power INTEGER,
 average_dps DOUBLE DEFAULT 0,
-total_damage INT DEFAULT 0,
+total_damage INTEGER DEFAULT 0,
 save_date DATETIME DEFAULT CURRENT_TIMESTAMP,
 script BLOB,
 FOREIGN KEY (kinetic)
@@ -73,9 +73,9 @@ ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE saved_sims_times (
-      id INT,
+      id INTEGER,
       sim_timestamp DOUBLE CHECK (sim_timestamp >= 0),
-      damage_instance INT DEFAULT 0 CHECK (damage_instance >= 0),
+      damage_instance INTEGER DEFAULT 0 CHECK (damage_instance >= 0),
       FOREIGN KEY (id)
           REFERENCES sims (id)
           ON DELETE CASCADE ON UPDATE CASCADE,
@@ -83,8 +83,8 @@ CREATE TABLE saved_sims_times (
 );
 
 CREATE TABLE sim_time_events (
-     id INT AUTO_INCREMENT PRIMARY KEY,
-     save_id INT,
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     save_id INTEGER,
      sim_timestamp DOUBLE,
      sim_event VARCHAR(64) NOT NULL,
      FOREIGN KEY (save_id , sim_timestamp)
@@ -93,9 +93,9 @@ CREATE TABLE sim_time_events (
 );
 
 CREATE TABLE pulse_rifle_specifics (
-           weapon_id INT,
-           burst_count INT DEFAULT 3 CHECK (burst_count > 0),
-           burst_recovery INT DEFAULT 300 CHECK (burst_recovery > 0),
+           weapon_id INTEGER,
+           burst_count INTEGER DEFAULT 3 CHECK (burst_count > 0),
+           burst_recovery INTEGER DEFAULT 300 CHECK (burst_recovery > 0),
            FOREIGN KEY (weapon_id)
                REFERENCES weapons (weapon_id)
                ON DELETE CASCADE ON UPDATE CASCADE,
@@ -103,10 +103,10 @@ CREATE TABLE pulse_rifle_specifics (
 );
 
 CREATE TABLE fusion_rifle_specifics (
-            weapon_id INT,
-            charge_time INT DEFAULT 1000 CHECK (charge_time > 0),
-            bolt_count INT DEFAULT 5 CHECK (bolt_count > 0),
-            charge_recovery INT DEFAULT 500 CHECK (charge_recovery > 0),
+            weapon_id INTEGER,
+            charge_time INTEGER DEFAULT 1000 CHECK (charge_time > 0),
+            bolt_count INTEGER DEFAULT 5 CHECK (bolt_count > 0),
+            charge_recovery INTEGER DEFAULT 500 CHECK (charge_recovery > 0),
             FOREIGN KEY (weapon_id)
                 REFERENCES weapons (weapon_id)
                 ON DELETE CASCADE ON UPDATE CASCADE,
@@ -114,8 +114,8 @@ CREATE TABLE fusion_rifle_specifics (
 );
 
 CREATE TABLE ammo_types (
-    weapon_id INT,
-    ammo INT,
+    weapon_id INTEGER,
+    ammo INTEGER,
     FOREIGN KEY (weapon_id)
         REFERENCES weapons (weapon_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
